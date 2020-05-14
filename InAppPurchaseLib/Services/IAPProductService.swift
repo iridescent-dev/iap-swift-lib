a6ff00a00ddf5c701d113c7e2fbc4035879b042a
Veronique <veronique@iridescent.dev>
2020-05-14 20:05:56 +0200

    refactoring Product extension -> localized period and duration

--- InAppPurchaseLib/Services/IAPProductService.swift
+++ InAppPurchaseLib/Services/IAPProductService.swift
@@ -93,48 +93,53 @@ class IAPProductService: NSObject, SKProductsRequestDelegate {
 }
 
 
+/* MARK: - SKProduct Extension */
+enum IAPPeriodFormat {
+    case short
+    case long
+}
+
 extension SKProduct {
+    static var localizedPeriodFormat: IAPPeriodFormat = .short
+    
     // Checks if the product has an introductory price the user is eligible to.
     func hasIntroductoryPriceEligible() -> Bool {
         let ineligibleForIntroPriceProductIDs = IAPStorageService.getStringArray(forKey: INELIGIBLE_FOR_INTRO_PRICE_PRODUCT_IDS_KEY)
         return !ineligibleForIntroPriceProductIDs.contains(productIdentifier) && introductoryPrice != nil
     }
     
-    // Returns a localized string with the current cost of the product, with reduction if available, in the local currency.
-    var localizedCurrentPrice: String {
-        if hasIntroductoryPriceEligible() {
-            return getLocalizedPrice(locale: introductoryPrice!.priceLocale, price: introductoryPrice!.price)
-        } else {
-            return getLocalizedPrice(locale: priceLocale, price: price)
-        }
-    }
-    
-    // Returns a localized string with the current period of the subscription product.
-    var localizedCurrentPeriod: String? {
-        let period = hasIntroductoryPriceEligible() ? introductoryPrice!.subscriptionPeriod : subscriptionPeriod
-        return getLocalizedSubscriptionPeriod(subscriptionPeriod: period)
-    }
-    
-    // Returns a localized string with the initial cost of the product in the local currency.
-    var localizedInitialPrice: String {
+    // Returns a localized string with the cost of the product in the local currency.
+    var localizedPrice: String {
         return getLocalizedPrice(locale: priceLocale, price: price)
     }
     
-    // Returns a localized string with the initial period of the subscription product.
-    var localizedInitialPeriod: String? {
-        return getLocalizedSubscriptionPeriod(subscriptionPeriod: subscriptionPeriod)
+    // Returns a localized string with the period of the subscription product.
+    var localizedSubscriptionPeriod: String? {
+        if subscriptionPeriod == nil { return nil }
+        return getLocalizedPeriod(unit: subscriptionPeriod!.unit, numberOfUnits: subscriptionPeriod!.numberOfUnits)
     }
     
-    // Returns a localized string with the period of the introductory price.
-    var localizedIntroductoryPricePeriod: String? {
-        if hasIntroductoryPriceEligible() {
-            return getLocalizedPeriod(unit: introductoryPrice!.subscriptionPeriod.unit, numberOfUnits: introductoryPrice!.numberOfPeriods)
-        }
-        return nil
+    // Returns a localized string with the introductory price if available, in the local currency.
+    var localizedIntroductoryPrice: String? {
+        if introductoryPrice == nil { return nil }
+        return getLocalizedPrice(locale: introductoryPrice!.priceLocale, price: introductoryPrice!.price)
     }
     
+    // Returns a localized string with the introductory price period of the subscription product.
+    var localizedIntroductoryPeriod: String? {
+        if introductoryPrice == nil { return nil }
+        return getLocalizedPeriod(unit: introductoryPrice!.subscriptionPeriod.unit, numberOfUnits: introductoryPrice!.subscriptionPeriod.numberOfUnits)
+        
+    }
     
-    /* MARK: - Private method. Returns formatted product Price and Period */
+    // Returns a localized string with the duration of the introductory price.
+    var localizedIntroductoryDuration: String? {
+        if introductoryPrice == nil { return nil }
+        let numberOfUnits = introductoryPrice!.subscriptionPeriod.numberOfUnits * introductoryPrice!.numberOfPeriods
+        return getLocalizedDuration(unit: introductoryPrice!.subscriptionPeriod.unit, numberOfUnits: numberOfUnits)
+    }
+    
+    /* MARK: - Private method. Returns formatted product Price, Period and Duration. */
     private func getLocalizedPrice(locale: Locale, price: NSDecimalNumber) -> String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
@@ -142,18 +147,22 @@ extension SKProduct {
         return formatter.string(from: price)!
     }
     
-    private func getLocalizedSubscriptionPeriod(subscriptionPeriod: SKProductSubscriptionPeriod?) -> String? {
-        if subscriptionPeriod != nil {
-            return getLocalizedPeriod(unit: subscriptionPeriod!.unit, numberOfUnits: subscriptionPeriod!.numberOfUnits, dropOne: true)
+    private func getLocalizedPeriod(unit: SKProduct.PeriodUnit, numberOfUnits: Int) -> String? {
+        let period = getLocalizedDuration(unit: unit, numberOfUnits: numberOfUnits)
+        switch SKProduct.localizedPeriodFormat {
+        case .long:
+            return period
+        case .short:
+            return period.replacingOccurrences(of: "^1 ", with: "", options: .regularExpression, range: nil)
         }
-        return nil
     }
     
-    private func getLocalizedPeriod(unit: SKProduct.PeriodUnit, numberOfUnits: Int, dropOne: Bool = false) -> String {
+    private func getLocalizedDuration(unit: SKProduct.PeriodUnit, numberOfUnits: Int) -> String {
         let formatter = DateComponentsFormatter()
         formatter.maximumUnitCount = 1
         formatter.unitsStyle = .full
         formatter.zeroFormattingBehavior = .dropAll
+        formatter.formattingContext = .middleOfSentence
         
         let calendarUnit = unit.toCalendarUnit()
         var dateComponents = DateComponents()
@@ -161,21 +170,26 @@ extension SKProduct {
         formatter.allowedUnits = [calendarUnit]
         
         switch calendarUnit {
-        case .day: dateComponents.setValue(numberOfUnits, for: .day)
-        case .weekOfMonth: dateComponents.setValue(numberOfUnits, for: .weekOfMonth)
-        case .month: dateComponents.setValue(numberOfUnits, for: .month)
-        case .year: dateComponents.setValue(numberOfUnits, for: .year)
+        case .day:
+            dateComponents.setValue(numberOfUnits, for: .day)
+            if numberOfUnits == 7 {
+                formatter.allowedUnits = [.weekOfMonth]
+            }
+        case .weekOfMonth:
+            dateComponents.setValue(numberOfUnits, for: .weekOfMonth)
+        case .month:
+            dateComponents.setValue(numberOfUnits, for: .month)
+            if numberOfUnits == 12 {
+                formatter.allowedUnits = [.year]
+            }
+        case .year:
+            dateComponents.setValue(numberOfUnits, for: .year)
         default:
             debugPrint("Unknown period unit")
             return ""
         }
         
-        var period = formatter.string(from: dateComponents)!
-        
-        if numberOfUnits == 1 && dropOne {
-            period = period.replacingOccurrences(of: "1", with: "").trimmingCharacters(in: .whitespacesAndNewlines)
-        }
-        return period
+        return formatter.string(from: dateComponents)!
     }
 }
 
